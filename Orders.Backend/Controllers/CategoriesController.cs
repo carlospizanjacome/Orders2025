a9331a781aedc130ee33d9b27b875aa7e486b3a3
MARIO HASSAN SALEG <MARIO HASSAN SALEG@DESKTOP-2C6QS1T>
2024-05-06 17:39:45 -0500

    Ok con unidades de trabajo hasta video 11 ok

--- Orders.Backend/Controllers/CategoriesController.cs
+++ Orders.Backend/Controllers/CategoriesController.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Orders.Backend.Data;
 using Orders.Backend.UnitsOfWork.Interfaces;
 using Orders.Shared.Entities;
 
 namespace Orders.Backend.Controllers
 {
+
     [ApiController]
     [Route("api/[controller]")]
-    public class CountriesController : GenericController<Country>
+
+    public class CategoriesController : GenericController<Category>
     {
-        public CountriesController(IGenericUnitOfWork<Country> unitOfWork) : base(unitOfWork)
+        public CategoriesController(IGenericUnitOfWork<Category> unitOfWork) : base(unitOfWork)
         {
         }
     }
-
 }
---
+++ Orders.Backend/UnitsOfWork/Interfaces/IGenericUnitsOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Orders.Shared.Entities.Responses;
+
+namespace Orders.Backend.UnitsOfWork.Interfaces
+{
+    public interface IGenericUnitOfWork<T> where T : class
+    {
+        Task<ActionResponse<IEnumerable<T>>> GetAsync();
+
+        Task<ActionResponse<T>> AddAsync(T model);
+
+        Task<ActionResponse<T>> UpdateAsync(T model);
+
+        Task<ActionResponse<T>> DeleteAsync(int Id);
+
+        Task<ActionResponse<T>> GetAsync(int Id);
+    }
+}
